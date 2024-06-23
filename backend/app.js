2ab4fd5a766f7becb2236e4547c9dcd62d6e1123
Yash Nishankar <yashnishankar3102001@gmail.com>
2024-06-23 21:25:22 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,13 @@ const routes = require('./routes/routes');
 const port = process.env.PORT
 
 app.use(cors({
-    origin: ['http://localhost:7000', 'https://nextjs-ott-platform.onrender.com'], // Add your domains here
+    origin: ['http://localhost:7000', 'https://nextjs-ott-platform.vercel.app'],
     methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
 }));
 
 require('./db/db');
 app.use(express.json());
-app.use(cors());
 app.use(routes);
 
 
